725f3e151a869b159e2f3006179397258a50d4f0
JiheeOh <jenny2596@naver.com>
2024-10-10 01:25:22 +0900

    2042 구간합 구하기 -> 시간 초과

--- Python_algorithm/Baekjun_Python/DataStructure/2042.py
+++ Python_algorithm/Baekjun_Python/DataStructure/2042.py
@@ -1,9 +1,17 @@
-# 구간 합 구하기
-n,m,k = int(input().split())
+# 구간 합 구하기 -> 배열 : 시간초과
+n,m,k = map(int,input().split())
 
 # n = 수의 개수 , m = 수의 변경이 일어나는 횟수 , k = 구간의 합을 구하는 횟수
 
-# 입력값으로 배열을 만들자
+# 입력값으로 배열 만들기
 data = []*n
 for i in range(n) :
-    data.append(input())
+    data.append(int(input()))
+
+# 배열의 변경
+for j in range(m+k):
+    a,b,c = map(int,input().split())
+    if a == 1 :
+        data[b-1] = c
+    elif a == 2 :
+        print(sum(data[b-1:c]))
