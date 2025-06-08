eecfd3d2b6b17e66c183c8c156e0aed10d1ae0df
JiheeOh <75823407+JiheeOh@users.noreply.github.com>
2025-06-08 19:40:06 +0900

    Title: 만나는 그 순간, Time: 88ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250608/만나는 그 순간/the-moment-we-meet.java
@@ -0,0 +1,80 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    private static int[] A = new int[1000000];
+    private static int[] B = new int[1000000];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int answer = 0;
+        // A 움직임 기록
+        int status = 1;
+        for(int i = 0 ; i< n; i ++){
+            st = new StringTokenizer(br.readLine());
+            String direction = st.nextToken();
+            int move = Integer.parseInt(st.nextToken());
+            switch (direction){
+                case "R" :
+                    while(move >0){
+                        A[status]= A[status-1] +1;
+                        status++;
+                        move--;
+
+                    }
+                    break;
+
+                case "L":
+                    while(move >0){
+                        A[status]= A[status-1] -1;
+                        status++;
+                        move--;
+
+                    }
+                    break;
+            }
+
+        }
+
+        // B 움직임 기록
+        status = 1;
+        for(int i = 0 ; i< m; i ++){
+            st = new StringTokenizer(br.readLine());
+            String direction = st.nextToken();
+            int move = Integer.parseInt(st.nextToken());
+            switch (direction){
+                case "R" :
+                    while(move >0){
+                        B[status]= B[status-1] +1;
+                        if (A[status] == B[status]){
+                            answer = status;
+                            break;
+                        }
+                        status++;
+                        move--;
+
+                    }
+                    break;
+
+                case "L":
+                    while(move >0){
+                        B[status]= B[status-1] -1;
+                        if (A[status] == B[status]){
+                            answer = status;
+                            break;
+                        }
+                        status++;
+                        move--;
+
+                    }
+                    break;
+            }
+
+        }
+        System.out.println(answer);
+    }
+}
