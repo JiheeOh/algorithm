7e1fba54f7675a5fed92f983bdb29d4db9e1f6d1
JiheeOh <75823407+JiheeOh@users.noreply.github.com>
2025-07-08 12:48:44 +0900

    Title: 오목, Time: 104ms, Memory: 9MB, Status: Wrong Answer - Codetree

---
+++ 250708/오목/O-mok.java
@@ -0,0 +1,80 @@
+
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Objects;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    private static int n = 19;
+    private static int[][] arr = new int[n][n];
+
+    public static void main(String[] args) throws IOException {
+
+        // 데이터 입력
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                int data = Integer.parseInt(st.nextToken());
+                if (data != 0) {
+                    arr[i][j] = data;
+                }
+            }
+        }
+
+        // 이기는 지 확인
+        boolean win = false;
+        int result = 0;
+        int row = 0;
+        int column =0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - 5; j++) {
+                // 가로 확인
+                if (arr[i][j] != 0 && arr[i][j] == arr[i][j + 1] && arr[i][j] == arr[i][j + 2]
+                        && arr[i][j] == arr[i][j + 3] && arr[i][j] == arr[i][j + 4]) {
+                    win = true;
+                    result = arr[i][j];
+                    row = i+1;
+                    column = j+3;
+
+                    break;
+                }
+                // 세로 확인
+                if (arr[i][j] != 0 && arr[j][i] == arr[j + 1][i] && arr[i][j] == arr[j + 2][i]
+                        && arr[i][j] == arr[j + 3][i] && arr[i][j] == arr[j + 4][i]) {
+                    win = true;
+                    result = arr[j][i];
+                    row = j+3;
+                    column = i+1;
+                    break;
+                }
+
+            }
+            if (win) {
+                break;
+            }
+        }
+        if (!win) {
+            for (int i = 0; i < n - 5; i++) {
+                if (arr[i][i] != 0 && arr[i][i] == arr[i + 1][i + 1] && arr[i][i] == arr[i + 2][i + 2]
+                        && arr[i][i] == arr[i + 3][i + 3] && arr[i][i] == arr[i + 4][i + 4]) {
+                    win = true;
+                    result = arr[i][i];
+                    row = i+3;
+                    column = i+3;
+                    break;
+                }
+            }
+        }
+        System.out.println(result);
+        if(result!=0){
+            System.out.println(String.format("%d %d",row,column));
+        }
+    }
+
+}
