3ae5e1093840a8c5b3c669e5c217ef360ec4f1ab
JiheeOh <75823407+JiheeOh@users.noreply.github.com>
2025-07-10 12:56:11 +0900

    Title: G or H 3, Time: 114ms, Memory: 9MB, Status: Wrong Answer - Codetree

---
+++ 250710/G or H 3/G-or-H-3.java
@@ -0,0 +1,49 @@
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        // 데이터 입력
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        String[] arr = new String[100000];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int status = Integer.parseInt(st.nextToken());
+            arr[status-1] = st.nextToken();
+        }
+
+        // 2개씩 빼서 계산
+        int result = Integer.MIN_VALUE;
+        for (int i = 0; i < n -k+ 1; i++) {
+            int score =0;
+            for(int j = 0; j<i+k+1;j++){
+                if(arr[i+j]==null){
+                    continue;
+                }
+                switch (arr[i+j]){
+                    case "G":
+                        score++;
+                        break;
+                    case "H":
+                        score = score+2;
+                        break;
+                }
+            }
+            if (score > result) {
+                result = score;
+            }
+        }
+        System.out.println(result);
+    }
+
+}
