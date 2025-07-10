8bf6e722b03aa5537b990de64c35777db66a9d5b
JiheeOh <75823407+JiheeOh@users.noreply.github.com>
2025-07-10 22:09:34 +0900

    Title: G or H 3, Time: 280ms, Memory: 10MB, Status: Passed - Codetree

--- 250710/G or H 3/G-or-H-3.java
+++ 250710/G or H 3/G-or-H-3.java
@@ -1,5 +1,3 @@
-
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -15,27 +13,34 @@ public class Main {
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
 
-        String[] arr = new String[100000];
+        String[] arr = new String[100001];
+        int maxStatus = 0;
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             int status = Integer.parseInt(st.nextToken());
-            arr[status-1] = st.nextToken();
+            arr[status] = st.nextToken();
+            if (status > maxStatus) {
+                maxStatus = status;
+            }
         }
 
         // 2개씩 빼서 계산
         int result = Integer.MIN_VALUE;
-        for (int i = 0; i < n -k+ 1; i++) {
-            int score =0;
-            for(int j = 0; j<i+k+1;j++){
-                if(arr[i+j]==null){
+        for (int i = 1; i < arr.length - k - 1; i++) {
+            int score = 0;
+            if (i > maxStatus) {
+                break;
+            }
+            for (int j = 0; j < k + 1; j++) {
+                if (arr[i + j] == null) {
                     continue;
                 }
-                switch (arr[i+j]){
+                switch (arr[i + j]) {
                     case "G":
                         score++;
                         break;
                     case "H":
-                        score = score+2;
+                        score = score + 2;
                         break;
                 }
             }
