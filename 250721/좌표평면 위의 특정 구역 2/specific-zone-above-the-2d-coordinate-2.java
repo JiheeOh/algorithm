5d9084752e3d23044789f192d41b9ab5837947aa
JiheeOh <75823407+JiheeOh@users.noreply.github.com>
2025-07-21 22:35:44 +0900

    Title: 좌표평면 위의 특정 구역 2, Time: 89ms, Memory: 9MB, Status: Passed - Codetree

--- 250721/좌표평면 위의 특정 구역 2/specific-zone-above-the-2d-coordinate-2.java
+++ 250721/좌표평면 위의 특정 구역 2/specific-zone-above-the-2d-coordinate-2.java
@@ -21,12 +21,15 @@ public class Main {
         }
 
         int result = Integer.MAX_VALUE;
-        for (int i = 0; i < n - (n - 2); i++) {
+        for (int i = 0; i < n ; i++) {
             List<Integer> xCollect = new ArrayList<>();
             List<Integer> yCollect = new ArrayList<>();
-            for (int j = 0; j < n - 1; j++) {
-                xCollect.add(x[i+j]);
-                yCollect.add(y[i+j]);
+            for (int j = 0; j < n; j++) {
+                if(j ==i){
+                    continue;
+                }
+                xCollect.add(x[j]);
+                yCollect.add(y[j]);
 
             }
             int width = (Collections.max(xCollect)-Collections.min(xCollect)) * (Collections.max(yCollect)-Collections.min(yCollect));
