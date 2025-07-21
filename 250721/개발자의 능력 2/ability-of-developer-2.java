e1c178401d14ee3493a2fbb74b0e6acb571af5e2
JiheeOh <75823407+JiheeOh@users.noreply.github.com>
2025-07-21 12:43:15 +0900

    Title: 개발자의 능력 2, Time: 234ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250721/개발자의 능력 2/ability-of-developer-2.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = 6;
+        int[] arr =new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int totalSum=0;
+        for(int i =0; i<n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+            totalSum += arr[i];
+        }
+
+        int minDiff = Integer.MAX_VALUE;
+        for(int i =0; i<n-1; i++){
+            for(int j = i+1;j<n; j++){
+                int sum1 = arr[i]+arr[j];
+                for(int k = 0; k<n-1; k++){
+                    if(k ==i || k== j){
+                        continue;
+                    }
+                    for(int q=k+1; q< n ; q++){
+                        if(q ==i || q ==j){
+                            continue;
+                        }
+                        int sum2 = arr[k]+arr[q];
+                        int sum3 = Math.abs(totalSum-(sum1+sum2));
+                        int[] numbers = {sum1,sum2,sum3};
+                        int maxSum = Arrays.stream(numbers).max().getAsInt();
+                        int minSum = Arrays.stream(numbers).min().getAsInt();
+                        int sumDiff = maxSum-minSum;
+                        if(minDiff > sumDiff){
+                            minDiff = sumDiff;
+                        }
+                    }
+                }
+            }
+        }
+        System.out.println(minDiff);
+    }
+}
