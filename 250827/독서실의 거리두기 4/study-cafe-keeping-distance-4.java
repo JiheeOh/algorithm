d384a79d4c19aab8fb796f59c8a55954a673a1ed
JiheeOh <75823407+JiheeOh@users.noreply.github.com>
2025-08-27 00:03:39 +0900

    Title: 독서실의 거리두기 4, Time: 94ms, Memory: 9MB, Status: Wrong Answer - Codetree

---
+++ 250827/독서실의 거리두기 4/study-cafe-keeping-distance-4.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        char[] data = br.readLine().toCharArray();
+
+        List<Integer> seat = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            if (data[i] == '1') {
+                seat.add(i);
+            }
+        }
+
+        int result = Integer.MIN_VALUE;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (data[i] != '0' || data[j] != '0') {
+                    continue;
+                }
+                List<Integer> seatTmp = new ArrayList<>(seat);
+                seatTmp.add(i);
+                seatTmp.add(j);
+                Collections.sort(seatTmp);
+                int distance = Integer.MAX_VALUE;
+                for (int k = 0; k < seatTmp.size()-2; k++) {
+                    int tmp = seatTmp.get(k + 1) - seatTmp.get(k);
+                    distance = Math.min(distance, tmp);
+                }
+                result = Math.max(result, distance);
+            }
+        }
+        System.out.println(result);
+    }
+
+
+}
