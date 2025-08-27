6332e47f7ee9551132cd96477ebbbe5cedacaaab
JiheeOh <75823407+JiheeOh@users.noreply.github.com>
2025-08-27 23:27:04 +0900

    Title: 이상한 폭탄 3, Time: 96ms, Memory: 9MB, Status: Wrong Answer - Codetree

---
+++ 250827/이상한 폭탄 3/strange-bomb-3.java
@@ -0,0 +1,42 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        int bangMax = 0;
+        int bombNm = -1;
+        for (int i = 0; i < n - 1; i++) {
+            int bangCnt = 0;
+            for (int j = 1; j < k + 1; j++) {
+                if (i + j >= n) {
+                    break;
+                }
+                if (arr[i] == arr[i + j]) {
+                    bangCnt++;
+                }
+            }
+                
+            if (bangCnt > bangMax) {
+                bangMax = bangCnt;
+                bombNm = arr[i];
+            }
+        }
+        System.out.println(bombNm);
+    }
+
+
+}
