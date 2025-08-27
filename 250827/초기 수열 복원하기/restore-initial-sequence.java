404ca4b837512fb2e24ec0e714940091b78c3e4d
JiheeOh <75823407+JiheeOh@users.noreply.github.com>
2025-08-27 12:51:42 +0900

    Title: 초기 수열 복원하기, Time: 94ms, Memory: 9MB, Status: Wrong Answer - Codetree

---
+++ 250827/초기 수열 복원하기/restore-initial-sequence.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] data = new int[n - 1];
+
+        if (n == 1) {
+            System.out.println(Integer.parseInt(br.readLine()));
+            return;
+        }
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n - 1; i++) {
+            data[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 1; i < data[0]; i++) { // 첫번째 값 선택
+            List<Integer> answerArr = new ArrayList<>();
+            answerArr.add(i);
+            for (int j = 1; j < n; j++) {
+                int tmp = data[j - 1] - answerArr.get(answerArr.size() - 1);
+                if (tmp == answerArr.get(answerArr.size() - 1)) {
+                    break;
+                } else {
+                    answerArr.add(tmp);
+                }
+            }
+            if (answerArr.size() == n) {
+                for (int k = 0; k < n; k++) {
+                    System.out.print(answerArr.get(k)+" ");
+                }
+                break;
+            }
+        }
+
+    }
+
+
+}
