9c18a81cb94b50ee439ead2a8208cdde7db3dca1
JiheeOh <75823407+JiheeOh@users.noreply.github.com>
2025-09-10 12:21:01 +0900

    Title: 두 선분, Time: 92ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250910/두 선분/two-lines.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int x1 = Integer.parseInt(st.nextToken());
+        int x2 = Integer.parseInt(st.nextToken());
+        int x3 = Integer.parseInt(st.nextToken());
+        int x4 = Integer.parseInt(st.nextToken());
+
+        if (x2 < x3 || x4 < x1) {
+            System.out.println("nonintersecting");
+        } else {
+            System.out.println("intersecting");
+        }
+    }
+
+
+}
