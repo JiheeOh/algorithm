177e00e5d3cff236cfa5d2d318b8e55133ff6f2b
JiheeOh <75823407+JiheeOh@users.noreply.github.com>
2025-09-12 00:24:46 +0900

    Title: 코딩톡, Time: 87ms, Memory: 9MB, Status: Runtime Error - Codetree

---
+++ 250912/코딩톡/coding-talk.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int p = Integer.parseInt(st.nextToken());
+
+        List<String> people = new ArrayList<String>();
+        int[] notReadList = new int[m];
+        String[] data = new String[m];
+        for (int i = 0; i < n; i++) {
+            people.add(String.valueOf((char) (i + 65)));
+        }
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            String person = st.nextToken();
+            int notRead = Integer.parseInt(st.nextToken());
+            notReadList[i] =notRead;
+            if (i == p - 1 && notRead == 0) {
+                people = new ArrayList<>();
+            }
+            data[i] = person;
+            if (i >= p - 1) {
+                if(notReadList[i] == notReadList[i-1]){
+                    people.remove(data[i-1]);
+                }
+                people.remove(person);
+            }
+        }
+
+        for(String person : people){
+            System.out.print(person+" ");
+        }
+
+    }
+
+
+}
