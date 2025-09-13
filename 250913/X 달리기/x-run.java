412e82be2e452c2ac98aec5cb0815e974cd3e2a6
JiheeOh <75823407+JiheeOh@users.noreply.github.com>
2025-09-13 20:00:11 +0900

    Title: X 달리기, Time: 90ms, Memory: 9MB, Status: Wrong Answer - Codetree

---
+++ 250913/X 달리기/x-run.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int distance = Integer.parseInt(br.readLine());
+
+        int result = Integer.MAX_VALUE;
+        for(int i= 1; i<distance;i++){
+            int fast = 0;
+            int go = 0;
+            int time = 0;
+            while(go <= 9){
+                if(time < i ){
+                    fast++;
+                }else{
+                    if(fast>1){
+                        fast--;
+                    }
+                }
+                go+= fast;
+                time++;
+            }
+            if (fast == 1){
+                result = Math.min(result,time);
+            }else{
+                break;
+            }
+        }
+        System.out.println(result);
+    }
+
+
+}
