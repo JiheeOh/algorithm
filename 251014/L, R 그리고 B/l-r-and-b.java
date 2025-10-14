8e7ea2c1fc5f146b4b2dbc27bad12e837ce8b836
JiheeOh <75823407+JiheeOh@users.noreply.github.com>
2025-10-14 12:37:02 +0900

    Title: L, R 그리고 B, Time: 88ms, Memory: 9MB, Status: Wrong Answer - Codetree

--- 251014/L, R 그리고 B/l-r-and-b.java
+++ 251014/L, R 그리고 B/l-r-and-b.java
@@ -12,21 +12,27 @@ public class Main {
 
         int[] B = new int[2];
         int[] L = new int[2];
+        int[] R = new int[2];
         for (int i = 0; i < 10; i++) {
             String[] data = br.readLine().split("");
             for (int j = 0; j < 10; j++) {
-                if(data[j].equals("B")){
+                if (data[j].equals("B")) {
                     B[0] = i;
                     B[1] = j;
                 } else if (data[j].equals("L")) {
                     L[0] = i;
                     L[1] = j;
+                } else if (data[j].equals("R")) {
+                    R[0] = i;
+                    R[1] = j;
                 }
             }
         }
-
-        System.out.println(Math.abs(L[0]-B[0]) + Math.abs(L[1]-B[1])-1);
-
+        int result = Math.abs(L[0] - B[0]) + Math.abs(L[1] - B[1]) - 1;
+        if (L[0] == B[0] && B[0] == R[0] || L[1] == B[1] && B[1] == R[1]) {
+            result += 2;
+        }
+        System.out.println(result);
 
     }
 }
