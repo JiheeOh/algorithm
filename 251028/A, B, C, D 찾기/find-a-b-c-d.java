af554a3d0e5360415d8213ea7a3e40986a100054
JiheeOh <75823407+JiheeOh@users.noreply.github.com>
2025-10-28 12:29:36 +0900

    Title: A, B, C, D 찾기, Time: 93ms, Memory: 9MB, Status: Wrong Answer - Codetree

---
+++ 251028/A, B, C, D 찾기/find-a-b-c-d.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int[] arr= new int[15];
+        for(int i = 0 ; i<15 ; i ++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        int a = arr[0];
+        int b = arr[1];
+        int c = 0;
+        int d = 0;
+        for(int i = 2; i <15 ;i++){
+            if(arr[i] <= a+b && arr[14]-(a+b+arr[i]) >= arr[i]){
+                c = arr[i];
+                d = arr[14]-(a+b+c);
+            }
+        }
+
+        System.out.printf("%d %d %d %d",a,b,c,d);
+    }
+}
