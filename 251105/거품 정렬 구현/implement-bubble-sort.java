468307e783bfa00831777aa9d81bb8c4b0549786
JiheeOh <75823407+JiheeOh@users.noreply.github.com>
2025-11-05 11:51:20 +0900

    Title: 거품 정렬 구현, Time: 96ms, Memory: 9MB, Status: Passed - Codetree

--- 251105/거품 정렬 구현/implement-bubble-sort.java
+++ 251105/거품 정렬 구현/implement-bubble-sort.java
@@ -15,7 +15,7 @@ public class Main {
         }
 
         for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - 2; j++) {
+            for (int j = 0; j < n - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
                     int tmp = arr[j];
                     arr[j] = arr[j + 1];
